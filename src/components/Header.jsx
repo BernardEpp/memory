ed783e4ed5fba6ff4d685a1ef98bcf07ca70e657
BernardEpp <bernard.epp@acommo.com>
2022-06-30 13:08:30 +0200

    fixed whitespace

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-const Header = ({  handleNewGame, wins}) => {
+const Header = ({ handleNewGame, wins}) => {
 
     useEffect(() => (document.title = `${wins} wins`), [wins]);
 
